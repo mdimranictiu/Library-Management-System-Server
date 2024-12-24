6b55a64e2d385d64706102c2459dfa2aa1e3d368
mdimranictiu <imranictiu11@gmail.com>
2024-12-24 18:52:04 +0600

    add book done

--- index.js
+++ index.js
@@ -1,15 +1,83 @@
-require('dotenv').config()
+require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
-const port= process.env.PORT || 5000;
-const app= express()
-
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-})
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nu3ic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+   
+
+    const booksCollection=client.db('Library').collection('books');
+ 
+// addBook
+    app.post('/addBook', async (req,res)=>{
+      const addBook= req.body;
+    console.log(addBook);
+    const result= await booksCollection.insertOne(addBook);
+    res.send(result);
+    })
+   
+
+    //GetAllBooks
+    app.get('/books',async (req,res)=>{
+      const cursor=booksCollection.find();
+      const result= await cursor.toArray();
+      res.send(result)
+    })
+   
+ 
+  
+ 
+   
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.listen(port,(req,res)=>{
-console.log(`server is running at http://localhost:${port}`);
+    console.log(`My server is running http://localhost:${port}`)
 })
